aceb106bfc9acbe69365f2d33828586fd98127c7
DerNikJakom <jan.komnik@googlemail.com>
2025-02-13 16:53:42 +0100

    Implement DeviceInformation component with input validation and navigation

--- src/app/device/page.tsx
+++ src/app/device/page.tsx
@@ -1,7 +1,92 @@
-import React from "react";
+"use client";
 
-const DevicePage = () => {
-  return <div>DevicePage</div>;
-};
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Button,
+  Box,
+  TextField,
+  Typography,
+} from "@mui/material";
 
-export default DevicePage;
+/* // TODO //
+- DB-Abfrage mit prisma
+- Card zentrieren
+- Bestätigen mit code Umleitung auf /device/[code]
+*/
+
+export default function DeviceInformation() {
+  const router = useRouter();
+
+  const [input, setInput] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const isHexCode = (code: string) => {
+    const regex = /^[0-9A-F]{6}$/i;
+    return regex.test(code);
+  };
+
+  return (
+    <Card sx={{ borderRadius: 3, backgroundColor: "#F2F7F8", width: 600 }}>
+      <CardHeader
+        title="Gerätecode eingeben"
+        subheader="6-stelliger Inventarcode"
+      />
+      <CardContent>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            error={!isHexCode(input) && input.length > 0}
+            helperText={
+              !isHexCode(input) && input.length > 0 && "kein gültiger Code"
+            }
+            required
+            autoFocus
+            onChange={handleChange}
+            value={input}
+            id="hex-input"
+            label="Inventarcode"
+            sx={{ color: "primary.main" }}
+          />
+        </Box>
+      </CardContent>
+      <CardActions
+        sx={{
+          alignSelf: "stretch",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+        }}
+      >
+        <Button
+          variant="outlined"
+          sx={{ color: "primary.main" }}
+          onClick={() => router.push("/")}
+        >
+          Zurück
+        </Button>
+        <Button
+          id="submit-code"
+          variant="contained"
+          sx={{ backgroundColor: "primary.main" }}
+          onClick={() => router.push("/device/123")}
+        >
+          Bestätigen
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
