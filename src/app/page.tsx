42219ac03e29701b7c2512193caf0042ad97e69c
DerNikJakom <jan.komnik@googlemail.com>
2025-02-13 12:00:21 +0100

    Refactor HomePage layout with Card component and integrate MenuButtons for improved user interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Card, CardContent, CardActions } from "@mui/material";
+import MenuButtons from "@/components/MenuButtons";
 
 export default function HomePage() {
   return (
-    <Box sx={{ bgcolor: "background.default", color: "text.primary", p: 3 }}>
-      <Typography variant="h4" color="primary">
-        Willkommen!
-      </Typography>
-      <Typography variant="body1" color="secondary">
-        Dies ist eine Beispielseite mit globalem Theme.
-      </Typography>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card
+        sx={{
+          borderRadius: 3,
+          justifySelf: "center",
+          backgroundColor: "background.default",
+          width: "50%",
+        }}
+      >
+        <CardContent sx={{ textAlign: "center" }}>
+          <Typography variant="h2" color="primary" gutterBottom>
+            Hallo [name]!
+          </Typography>
+          <Typography variant="h5" color="secondary">
+            Was möchtest du tun?
+          </Typography>
+        </CardContent>
+
+        <CardActions sx={{ justifyContent: "center" }}>
+          <MenuButtons />
+        </CardActions>
+      </Card>
     </Box>
   );
 }
