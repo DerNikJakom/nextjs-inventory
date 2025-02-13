83ea5275e46f736a47ccb34115309cb1c4ad0ade
DerNikJakom <jan.komnik@googlemail.com>
2025-02-13 12:00:04 +0100

    Add MenuButtons component for navigation and actions

---
+++ src/components/MenuButtons.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import { Stack, Button } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useRouter } from "next/navigation";
+
+export default function MenuButtons(props) {
+  const theme = useTheme(); // theme laden
+  const router = useRouter();
+
+  return (
+    <Stack direction="column" gap={1}>
+      <Button
+        id="deviceInfoBtn"
+        variant="contained"
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          color: theme.palette.text.primary,
+        }}
+        onClick={() => router.push("/device")}
+      >
+        Geräteinformation einsehen
+      </Button>
+      <Button
+        id="inventoryBtn"
+        variant="contained"
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          color: theme.palette.text.primary,
+        }}
+        onClick={() => router.push("/inventory")}
+      >
+        Inventar verwalten
+      </Button>
+      <Button
+        id="logOutBtn"
+        variant="contained"
+        sx={{ backgroundColor: theme.palette.primary.main }}
+      >
+        Ausloggen
+      </Button>
+    </Stack>
+  );
+}
