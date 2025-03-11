3102d14381a8f4061c67f8ccff49685b53a0924f
DerNikJakom <jan.komnik@googlemail.com>
2025-03-11 15:06:02 +0100

    Remove redundant hex code validation from device route

--- src/app/api/device/[code]/route.ts
+++ src/app/api/device/[code]/route.ts
@@ -7,12 +7,12 @@ export async function GET(
   request: Request,
   { params }: { params: { code: string } }
 ) {
-  // Überprüfen, ob der Code genau 6 Zeichen lang ist und nur Hexadezimalzeichen enthält
-  const codePattern = /^[0-9a-fA-F]{6}$/;
+  // Überprüfen, ob der Code genau 6 Zeichen lang ist und nur Hexadezimalzeichen enthält -> bereits im Frontend
+  // const codePattern = /^[0-9a-fA-F]{6}$/;
 
-  if (!codePattern.test(params.code)) {
-    return NextResponse.json({ error: "Invalid hex code" }, { status: 400 });
-  }
+  // if (!codePattern.test(params.code)) {
+  //   return NextResponse.json({ error: "Invalid hex code" }, { status: 400 });
+  // }
 
   // Suche nach dem Gerät anhand des Hex-Codes
   const device = await prisma.geraete.findUnique({
