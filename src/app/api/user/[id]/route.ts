7d2ad850a5cfee516b144d4c6bfcec054d93c725
DerNikJakom <jan.komnik@googlemail.com>
2025-03-18 13:57:04 +0100

    Enhance error handling in user API route by validating ID and adding try-catch for database queries

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -5,21 +5,34 @@ const prisma = new PrismaClient();
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = parseInt(params.id);
+  try {
+    const { id } = await params;
+    if (!id) {
+      return NextResponse.json("ID fehlt", { status: 400 });
+    }
 
-  if (isNaN(id)) {
-    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+    // Überprüfen, ob die ID eine gültige Zahl ist
+    if (isNaN(Number(id))) {
+      return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+    }
+
+    // Suche nach dem Mitarbeiter in der Datenbank
+    const user = await prisma.mitarbeiter.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("Fehler bei der Datenbankabfrage:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
-
-  const user = await prisma.mitarbeiter.findUnique({
-    where: { id: id },
-  });
-
-  if (!user) {
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
-  }
-
-  return NextResponse.json(user);
 }
