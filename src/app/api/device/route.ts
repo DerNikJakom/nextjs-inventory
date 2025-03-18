69810c7d723fd50b587bfd3c1745f18a581bb664
DerNikJakom <jan.komnik@googlemail.com>
2025-03-18 09:51:17 +0100

    Enhance GET endpoint to include employee data and handle errors gracefully

--- src/app/api/device/route.ts
+++ src/app/api/device/route.ts
@@ -4,6 +4,33 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const users = await prisma.geraete.findMany();
-  return NextResponse.json(users);
+  try {
+    // Suche nach allen Geräten und schließe die Mitarbeiterdaten ein
+    const devices = await prisma.geraete.findMany({
+      include: {
+        mitarbeiter: {
+          select: {
+            vorname: true,
+            nachname: true,
+          },
+        },
+      },
+    });
+
+    // // Transformiere die Daten, um nur die Attribute vorname und nachname einzuschließen
+    // const response = devices.map((device) => ({
+    //   ...device,
+    //   vorname: device.mitarbeiter?.vorname ?? null,
+    //   nachname: device.mitarbeiter?.nachname ?? null,
+    //   mitarbeiter: undefined, // Entferne das verschachtelte Mitarbeiter-Objekt
+    // }));
+
+    return NextResponse.json(devices);
+  } catch (error) {
+    console.error("Fehler bei der Datenbankabfrage:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
